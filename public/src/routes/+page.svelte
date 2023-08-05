bc08c333cbfeb694724124da25850ebb64fc0401
Alireza Saadatnia <alirezasn3@gmail.com>
2023-08-05 19:16:26 +0330

    general improvements

--- public/src/routes/+page.svelte
+++ public/src/routes/+page.svelte
@@ -1,17 +1,29 @@
 <script lang="ts">
-	import DashboardInfoStore from '../store';
+	import '../app.css';
 	import type { DashboardInfo, Peer } from '../types';
+	import { fade, fly } from 'svelte/transition';
 
 	let peers: Peer[] = [];
 	let groups: { [key: string]: Peer[] } = {};
-	let dashboardInfo: DashboardInfo;
+	let dashboardInfo: DashboardInfo = {
+		name: '',
+		isAdmin: false,
+		totalRx: 0,
+		totalTx: 0,
+		currentRx: 0,
+		currentTx: 0
+	};
 	let sortBy = 'expiry';
 	let sortOrder = 1;
 	let serch = '';
 	let currentPeer: Peer | null = null;
 	let view = 'peers';
-
-	DashboardInfoStore.subscribe((info) => (dashboardInfo = info));
+	let showCreatPeer = false;
+	let newName = '';
+	let newExpiry = '';
+	let editingName = false;
+	let editingExpiry = false;
+	let createPeerError = '';
 
 	$: {
 		peers = peers
@@ -29,31 +41,32 @@
 	}
 
 	setInterval(async () => {
-		const res = await fetch('/api/stats');
+		const res = await fetch('http://my.stats:5051/api/stats');
 		if (res.status === 200) {
 			const data = await res.json();
 			peers = Object.values(data.peers as Peer[]);
-			DashboardInfoStore.set({
+			if (currentPeer) currentPeer = data.peers[currentPeer.publicKey];
+			dashboardInfo = {
 				name: data.name,
 				isAdmin: data.isAdmin,
 				totalRx: data.totalRx,
 				totalTx: data.totalTx,
 				currentRx: data.currentRx,
 				currentTx: data.currentTx
-			} as DashboardInfo);
+			};
 		}
 	}, 1000);
 
-	function formatSeconds(totalSeconds: number) {
+	function formatSeconds(totalSeconds: number, noPrefix = false) {
 		if (!totalSeconds) return 'unknown';
 		totalSeconds = Math.trunc(totalSeconds - Date.now() / 1000);
-		const prefix = totalSeconds < 0 ? '-' : '';
+		const prefix = totalSeconds < 0 && !noPrefix ? '-' : '';
 		totalSeconds = Math.abs(totalSeconds);
 		if (totalSeconds / 60 < 1) return `${prefix}${totalSeconds} seconds`;
 		const totalMinutes = Math.trunc(totalSeconds / 60);
 		if (totalMinutes / 60 < 1) return `${prefix}${totalMinutes} minutes`;
 		const totalHours = Math.trunc(totalMinutes / 60);
-		if (totalHours / 60 < 1) return `${prefix}${totalHours} hours`;
+		if (totalHours / 24 < 1) return `${prefix}${totalHours} hours`;
 		return `${prefix}${Math.trunc(totalHours / 24)} days`;
 	}
 
@@ -68,142 +81,302 @@
 		if (totalGigas < 100) return `${totalGigas < 10 ? '0' : ''}${totalGigas.toFixed(2)} GB`;
 		return `${totalTeras < 10 ? '0' : ''}${totalTeras.toFixed(2)} TB`;
 	}
+
+	async function createPeer(name: string) {
+		try {
+			const res = await fetch('/api/peers/' + name, { method: 'POST' });
+			if (res.status === 200) {
+				const data = await res.json();
+				console.log(data);
+			}
+		} catch (error) {
+			console.log(error);
+			createPeerError = (error as Error).message;
+		}
+	}
 </script>
 
-{#if dashboardInfo.isAdmin}
-	<div class="mx-4 mt-16 border-b-2 border-slate-900 p-4 font-bold max-md:px-0 max-md:text-sm">
-		<div class="flex items-center">
-			<div>&#8595; {formatBytes(dashboardInfo.currentRx)}/S</div>
-			<div class="mx-3 h-1.5 w-1.5 rounded-full bg-slate-700" />
-			<div>{formatBytes(dashboardInfo.totalRx)}</div>
+<nav
+	class="fixed left-0 top-0 flex h-16 w-full items-center justify-between border-b-2 border-slate-900 bg-slate-950 p-4 text-lg font-bold"
+>
+	<span>Wireguard UI</span>
+	<span>{dashboardInfo.name}</span>
+</nav>
+<div class="mt-16">
+	{#if dashboardInfo.isAdmin}
+		<div class="mx-4 mt-16 border-b-2 border-slate-900 p-4 font-bold max-md:px-0 max-md:text-sm">
+			<div class="flex items-center">
+				<div>&#8595; {formatBytes(dashboardInfo.currentRx)}/S</div>
+				<div class="mx-3 h-1.5 w-1.5 rounded-full bg-slate-700" />
+				<div>{formatBytes(dashboardInfo.totalRx)}</div>
+			</div>
+			<div class="flex items-center">
+				<div>&#8593; {formatBytes(dashboardInfo.currentTx)}/S</div>
+				<div class="mx-3 h-1.5 w-1.5 rounded-full bg-slate-700" />
+				<div>{formatBytes(dashboardInfo.totalTx)}</div>
+			</div>
 		</div>
-		<div class="flex items-center">
-			<div>&#8593; {formatBytes(dashboardInfo.currentTx)}/S</div>
-			<div class="mx-3 h-1.5 w-1.5 rounded-full bg-slate-700" />
-			<div>{formatBytes(dashboardInfo.totalTx)}</div>
+		<div class="mx-8 my-4 flex items-center justify-between max-md:mx-4 max-md:text-sm">
+			<div>{peers.length} Peers</div>
+			<div>{Object.keys(groups).length} Groups</div>
 		</div>
-	</div>
-	<div class="mx-8 my-4 flex items-center justify-between max-md:mx-4 max-md:text-sm">
-		<div>{peers.length} Peers</div>
-		<div>{Object.keys(groups).length} Groups</div>
-	</div>
-{/if}
+		{#if currentPeer === null}
+			<button
+				on:click={() => (showCreatPeer = true)}
+				class="fixed bottom-6 right-6 flex h-12 w-12 items-center justify-center rounded-2xl bg-teal-700 text-lg font-bold hover:cursor-pointer hover:bg-teal-600"
+			>
+				<span class="absolute h-1 w-6 bg-white" />
+				<span class="absolute h-1 w-6 rotate-90 bg-white" />
+			</button>
+		{/if}
+	{/if}
 
-{#if peers.length}
-	{#if view === 'peers'}
-		<div class="md:m-4">
-			<table class="w-full table-auto bg-slate-900 text-left max-md:text-xs md:rounded-lg">
-				<thead class="border-b-2 border-slate-800">
-					<tr class="select-none">
-						<th class="p-2">#</th>
-						<th class="p-2">Name</th>
-						<th
-							on:click={() => {
-								sortBy = 'expiry';
-								if ((sortBy = 'expiry')) {
-									if (sortOrder < 0) sortOrder = 1;
-									else sortOrder = -1;
-								}
-							}}
-							class="p-2 hover:cursor-pointer hover:underline {sortBy === 'expiry' &&
-								'bg-gray-950 font-black'}"
-							>{sortBy === 'expiry' && sortOrder === 1 ? '↑' : sortBy === 'expiry' ? '↓' : ''} Expiry</th
-						>
-						<th
-							on:click={() => {
-								sortBy = 'bandwidth';
-								if ((sortBy = 'bandwidth')) {
-									if (sortOrder < 0) sortOrder = 1;
-									else sortOrder = -1;
-								}
-							}}
-							class="p-2 hover:cursor-pointer hover:underline {sortBy === 'bandwidth' &&
-								'bg-gray-950 font-black'}"
-							>{sortBy === 'bandwidth' && sortOrder === 1 ? '↑' : sortBy === 'bandwidth' ? '↓' : ''}
-							Bandwidth</th
-						>
-						<th
-							on:click={() => {
-								sortBy = 'usage';
-								if ((sortBy = 'usage')) {
-									if (sortOrder < 0) sortOrder = 1;
-									else sortOrder = -1;
-								}
-							}}
-							class="p-2 hover:cursor-pointer hover:underline {sortBy === 'usage' &&
-								'bg-gray-950 font-black'}"
-							>{sortBy === 'usage' && sortOrder === 1 ? '↑' : sortBy === 'usage' ? '↓' : ''} Usage</th
-						>
-					</tr>
-				</thead>
-				<tbody
-					class="hover:cursor-pointer [&>*:nth-child(even)]:border-y-[1px] [&>*:nth-child(even)]:border-slate-800"
-				>
-					{#each peers as peer, i}
-						<tr
-							on:click={() => {
-								currentPeer = peer;
-								document.body.style.overflowY = 'hidden';
-							}}
-							class="{Math.trunc(peer.expiresAt - Date.now() / 1000) < 0 &&
-								'text-red-500'} hover:bg-slate-800"
-						>
-							<td class="px-2 py-1">{i + 1}</td>
-							<td class="px-2 py-1">{peer.name}</td>
-							<td class="px-2 py-1 {sortBy === 'expiry' && 'bg-gray-950 font-black'}">
-								<span class="hidden max-md:block"
-									>{formatSeconds(peer.expiresAt).replace(' ', '')}</span
-								>
-								<span class="hidden md:block">{formatSeconds(peer.expiresAt)}</span>
-							</td>
-							<td class="px-2 py-1 {sortBy === 'bandwidth' && 'bg-gray-950 font-black'}"
-								><span class="hidden max-md:block"
-									>{formatBytes(peer.currentRx).replace(' ', '')}</span
-								>
-								<span class="hidden md:block">{formatBytes(peer.currentRx)}</span></td
+	{#if peers.length}
+		{#if view === 'peers'}
+			<div class="md:m-4">
+				<table class="w-full table-auto bg-slate-900 text-left max-md:text-xs md:rounded-lg">
+					<thead class="border-b-2 border-slate-800">
+						<tr class="select-none">
+							<th class="p-2">#</th>
+							<th class="p-2">Name</th>
+							<th
+								on:click={() => {
+									if (sortBy == 'expiry') {
+										if (sortOrder < 0) sortOrder = 1;
+										else sortOrder = -1;
+									}
+									sortBy = 'expiry';
+								}}
+								class="p-2 hover:cursor-pointer hover:underline {sortBy === 'expiry' &&
+									'bg-gray-950 font-black'}"
+								>{sortBy === 'expiry' && sortOrder === 1 ? '↑' : sortBy === 'expiry' ? '↓' : ''} Expiry</th
 							>
-							<td class="px-2 py-1 {sortBy === 'usage' && 'bg-gray-950 font-black'}"
-								><span class="hidden max-md:block"
-									>{formatBytes(peer.totalRx + peer.totalTx).replace(' ', '')}</span
-								>
-								<span class="hidden md:block">{formatBytes(peer.totalRx + peer.totalTx)}</span></td
+							<th
+								on:click={() => {
+									if (sortBy == 'bandwidth') {
+										if (sortOrder < 0) sortOrder = 1;
+										else sortOrder = -1;
+									}
+									sortBy = 'bandwidth';
+								}}
+								class="p-2 hover:cursor-pointer hover:underline {sortBy === 'bandwidth' &&
+									'bg-gray-950 font-black'}"
+								>{sortBy === 'bandwidth' && sortOrder === 1
+									? '↑'
+									: sortBy === 'bandwidth'
+									? '↓'
+									: ''}
+								Bandwidth</th
+							>
+							<th
+								on:click={() => {
+									if (sortBy == 'usage') {
+										if (sortOrder < 0) sortOrder = 1;
+										else sortOrder = -1;
+									}
+									sortBy = 'usage';
+								}}
+								class="p-2 hover:cursor-pointer hover:underline {sortBy === 'usage' &&
+									'bg-gray-950 font-black'}"
+								>{sortBy === 'usage' && sortOrder === 1 ? '↑' : sortBy === 'usage' ? '↓' : ''} Usage</th
 							>
 						</tr>
-					{/each}
-				</tbody>
-			</table>
-		</div>
+					</thead>
+					<tbody
+						class="hover:cursor-pointer [&>*:nth-child(even)]:border-y-[1px] [&>*:nth-child(even)]:border-slate-800"
+					>
+						{#each peers as peer, i}
+							<tr
+								on:click={() => {
+									currentPeer = peer;
+									document.body.style.overflowY = 'hidden';
+								}}
+								class="{Math.trunc(peer.expiresAt - Date.now() / 1000) < 0 &&
+									'text-red-500'} hover:bg-slate-800"
+							>
+								<td class="px-2 py-1">{i + 1}</td>
+								<td class="px-2 py-1">{peer.name}</td>
+								<td class="px-2 py-1 {sortBy === 'expiry' && 'bg-gray-950 font-black'}">
+									<span class="hidden max-md:block"
+										>{formatSeconds(peer.expiresAt).replace(' ', '')}</span
+									>
+									<span class="hidden md:block">{formatSeconds(peer.expiresAt)}</span>
+								</td>
+								<td class="px-2 py-1 {sortBy === 'bandwidth' && 'bg-gray-950 font-black'}"
+									><span class="hidden max-md:block"
+										>{formatBytes(peer.currentRx).replace(' ', '')}</span
+									>
+									<span class="hidden md:block">{formatBytes(peer.currentRx)}</span></td
+								>
+								<td class="px-2 py-1 {sortBy === 'usage' && 'bg-gray-950 font-black'}"
+									><span class="hidden max-md:block"
+										>{formatBytes(peer.totalRx + peer.totalTx).replace(' ', '')}</span
+									>
+									<span class="hidden md:block">{formatBytes(peer.totalRx + peer.totalTx)}</span
+									></td
+								>
+							</tr>
+						{/each}
+					</tbody>
+				</table>
+			</div>
+		{:else}
+			<div />
+		{/if}
 	{:else}
-		<div />
+		<div class="flex h-[calc(100vh-64px)] w-full items-center justify-center text-lg font-bold">
+			Loading...
+		</div>
 	{/if}
-{:else}
-	<div class="flex h-full w-full items-center justify-center text-lg font-bold">Loading...</div>
-{/if}
 
-{#if currentPeer !== null}
-	<div
-		class="fixed left-0 top-16 flex h-[calc(100vh-64px)] w-[100vw] items-center justify-center bg-slate-950 bg-opacity-95 p-4"
-	>
-		<div class="m-4 h-full max-h-[400px] w-full max-w-[400px] rounded-lg bg-slate-900">
-			<div class="border-b-2 border-slate-800 p-4">
-				<button
-					on:click={() => {
-						currentPeer = null;
-						document.body.style.overflowY = 'auto';
-					}}
-					class="text-2xl font-black">X</button
-				>
-			</div>
-			<div class="flex flex-col p-4">
-				<span>{currentPeer.name}</span>
-				<span>{currentPeer.allowedIps}</span>
-				<span>{currentPeer.totalRx}</span>
-				<span>{currentPeer.totalTx}</span>
-				<span>{currentPeer.currentRx}</span>
-				<span>{currentPeer.currentTx}</span>
-				<span>{currentPeer.latestHandshake}</span>
-				<span>{currentPeer.expiresAt}</span>
+	{#if currentPeer !== null}
+		<div
+			transition:fade={{ duration: 200 }}
+			class="fixed left-0 top-16 flex h-[calc(100vh-64px)] w-[100vw] items-center justify-center bg-slate-950 bg-opacity-95 p-4"
+		>
+			<div
+				transition:fly={{ y: 200, duration: 200 }}
+				class="m-4 h-full w-full rounded-lg bg-slate-900"
+			>
+				<div class="flex items-center justify-between rounded-t-lg bg-slate-800 px-8 py-2">
+					<div class="text-2xl font-black">{currentPeer.name}</div>
+					<button
+						on:click={() => {
+							currentPeer = null;
+							editingExpiry = false;
+							editingName = false;
+							document.body.style.overflowY = 'auto';
+						}}
+						class="relative h-12 w-12 rounded-2xl hover:cursor-pointer"
+					>
+						<span class="absolute h-1 w-8 rotate-45 rounded bg-white" />
+						<span class="absolute h-1 w-8 -rotate-45 rounded bg-white" />
+					</button>
+				</div>
+				<div class="flex flex-col p-4">
+					<div class="mb-4 flex justify-end border-b-2 border-slate-700 pb-4">
+						{#if !editingName}
+							<button
+								on:click={() => {
+									newName = currentPeer?.name || '';
+									editingName = true;
+									editingExpiry = false;
+								}}
+								class="ml-2 rounded bg-orange-500 px-2 py-1 font-bold max-md:text-sm"
+								>EDIT NAME</button
+							>
+						{/if}
+						{#if !editingExpiry}
+							<button
+								on:click={() => {
+									newExpiry = Math.trunc(
+										((currentPeer?.expiresAt || 0) - Date.now() / 1000) / (3600 * 24)
+									).toString();
+									editingExpiry = true;
+									editingName = false;
+								}}
+								class="ml-2 rounded bg-orange-500 px-2 py-1 font-bold max-md:text-sm"
+								>EDIT EXPIRY</button
+							>
+						{/if}
+						<button class="ml-2 rounded bg-red-500 px-2 py-1 font-bold max-md:text-sm"
+							>DELETE</button
+						>
+					</div>
+					{#if editingName}
+						<div class="mb-2">Editing Peer's Name</div>
+						<div class="flex w-full">
+							<input type="text" bind:value={newName} class="w-full rounded px-2 py-1 text-black" />
+							<button
+								on:click={() => {
+									editingName = false;
+								}}
+								class="ml-2 rounded bg-green-500 px-2 py-1 font-bold">SAVE</button
+							>
+						</div>
+					{:else if editingExpiry}
+						<div class="mb-2">Editing Peer's Expiry</div>
+						<div class="flex w-full items-center">
+							<input
+								type="text"
+								bind:value={newExpiry}
+								class="w-full rounded-l px-2 py-1 text-black outline-none"
+							/>
+							<div class="rounded-r bg-white px-2 py-1 text-black">days</div>
+							<button
+								on:click={() => {
+									editingExpiry = false;
+								}}
+								class="ml-2 rounded bg-green-500 px-2 py-1 font-bold">SAVE</button
+							>
+						</div>
+					{:else}
+						<div class="mb-2">
+							<div class="font-bold">Address:</div>
+							<div class="ml-4 text-sm text-slate-300">{currentPeer.allowedIps}</div>
+						</div>
+						<div class="mb-2">
+							<div class="font-bold">Bandwidth:</div>
+							<div class="ml-4 text-sm text-slate-300">
+								<span class="text-lg">↓</span>
+								{formatBytes(currentPeer.totalRx)}
+							</div>
+							<div class="ml-4 text-sm text-slate-300">
+								<span class="text-lg">↑</span>
+								{formatBytes(currentPeer.totalTx)}
+							</div>
+						</div>
+						<div class="mb-2">
+							<div class="font-bold">Usage:</div>
+							<div class="ml-4 text-sm text-slate-300">
+								<span class="text-lg">↓</span>
+								{formatBytes(currentPeer.currentRx)}
+							</div>
+							<div class="ml-4 text-sm text-slate-300">
+								<span class="text-lg">↑</span>
+								{formatBytes(currentPeer.currentTx)}
+							</div>
+						</div>
+						<div class="mb-2">
+							<div class="font-bold">Latest Handshake:</div>
+							<div class="ml-4 text-sm text-slate-300">
+								{formatSeconds(currentPeer.latestHandshake, true)} ago
+							</div>
+						</div>
+						<div class="mb-2">
+							<div class="font-bold">Expiry:</div>
+							<div class="ml-4 text-sm text-slate-300">{formatSeconds(currentPeer.expiresAt)}</div>
+						</div>
+					{/if}
+				</div>
 			</div>
 		</div>
-	</div>
-{/if}
+	{/if}
+
+	{#if showCreatPeer}
+		<div
+			transition:fade={{ duration: 200 }}
+			class="fixed left-0 top-16 flex h-[calc(100vh-64px)] w-[100vw] items-center justify-center bg-slate-950 bg-opacity-95 p-4"
+		>
+			<div
+				transition:fly={{ y: 200, duration: 200 }}
+				class="m-4 h-full w-full rounded-lg bg-slate-900"
+			>
+				<div class="flex items-center justify-between border-b-2 border-slate-800 px-8 py-2">
+					<div class="text-2xl font-black">Crete Peer</div>
+					<button
+						on:click={() => {
+							showCreatPeer = false;
+							document.body.style.overflowY = 'auto';
+						}}
+						class="relative h-12 w-12 rounded-2xl hover:cursor-pointer"
+					>
+						<span class="absolute h-1 w-8 rotate-45 rounded bg-white" />
+						<span class="absolute h-1 w-8 -rotate-45 rounded bg-white" />
+					</button>
+				</div>
+				<div class="flex flex-col p-4" />
+			</div>
+		</div>
+	{/if}
+</div>
