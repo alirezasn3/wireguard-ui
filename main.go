3d3d21332b2c4d95c4408db051c4e0777adc69ad
Alireza Saadatnia <alirezasn3@gmail.com>
2024-01-17 00:02:25 +0330

    added debug logs

--- main.go
+++ main.go
@@ -529,8 +529,7 @@ func main() {
 				// check if message is command
 				if update.Message.Command() != "" {
 					// check if its add register command
-					fmt.Println(update.Message.Command())
-					if update.Message.Command() == "/start" {
+					if update.Message.Command() == "start" {
 						tt := update.Message.CommandArguments()
 						fmt.Println(tt)
 						// check if arg is peer's telegram token
